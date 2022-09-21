31c7a54da13a091c330d07540428ecd3b96030d3
Venom <vishangid@gmail.com>
2022-09-22 00:28:10 +0530

    Added : Welcome to hello world react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>hello from Bridgelabz !</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
