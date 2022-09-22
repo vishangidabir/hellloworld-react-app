08b2e9430d6a3495fff8b2c084f5e8d7e5d5e587
Venom <vishangid@gmail.com>
2022-09-22 16:50:30 +0530

    Added : To display Hello from BridgeLabz with a user input name with regex pattern

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      userName: ''
+      userName: '',
+      nameError: ''
     }
   }
 
@@ -19,12 +20,18 @@ class App extends React.Component {
   }
   onNameChange = (event) => {
     console.log("value is ", event.target.value);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
     // set the title using setState method
     this.setState({userName: event.target.value}) 
+    if(nameRegex.test(event.target.value)){
+      this.setState({ nameError: '' })
+    }else{
+      this.setState({nameError: 'Name is Incorrect !' })
+    }
   }
   render(){
     return(
-      <>
+      <div>
       <div>
         <h1>Hello {this.state.userName} from Bridgelabz</h1>
         <img src={logo} onClick={this.onclick}
@@ -32,8 +39,9 @@ class App extends React.Component {
       </div>
       <div>
         <input onChange={this.onNameChange}/>
+        <span className="error-output">{this.state.nameError}</span>
       </div>
-      </>
+      </div>
     );
   }
 }
